c8c9fc8fa4fa52d7ad5b38cb08497ea31060b406
omaraljashmi <omaraljashmi.uni@gmail.com>
2026-07-13 15:02:47 -0400

    Milestone 2: update ods/__init__.py

--- ods/__init__.py
+++ ods/__init__.py
@@ -1,14 +1,18 @@
 """Core analysis utilities for Open Data Scientist."""
 
 from .loader import DatasetLoadError, load_dataset
+from .dashboard import ChartSuggestion, build_chart_data, infer_column_roles, suggest_dashboard
 from .profiler import DatasetProfile, profile_dataset
 from .reporting import build_markdown_report
 
 __all__ = [
     "DatasetLoadError",
     "DatasetProfile",
+    "ChartSuggestion",
     "build_markdown_report",
+    "build_chart_data",
+    "infer_column_roles",
     "load_dataset",
     "profile_dataset",
+    "suggest_dashboard",
 ]
-
