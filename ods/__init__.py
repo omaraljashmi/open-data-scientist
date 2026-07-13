7e51b28dd6dc86d9299caee395bd8aa5adf56ba9
omaraljashmi <omaraljashmi.uni@gmail.com>
2026-07-13 15:31:24 -0400

    Milestone 2.1: export guided dashboard API

--- ods/__init__.py
+++ ods/__init__.py
@@ -1,7 +1,17 @@
 """Core analysis utilities for Open Data Scientist."""
 
 from .loader import DatasetLoadError, load_dataset
-from .dashboard import ChartSuggestion, build_chart_data, infer_column_roles, suggest_dashboard
+from .dashboard import (
+    ChartSuggestion,
+    ColumnRoles,
+    ColumnSemantic,
+    INTENTS,
+    build_chart_data,
+    infer_column_roles,
+    infer_column_semantics,
+    roles_from_mapping,
+    suggest_dashboard,
+)
 from .profiler import DatasetProfile, profile_dataset
 from .reporting import build_markdown_report
 
@@ -9,10 +19,15 @@ __all__ = [
     "DatasetLoadError",
     "DatasetProfile",
     "ChartSuggestion",
+    "ColumnRoles",
+    "ColumnSemantic",
+    "INTENTS",
     "build_markdown_report",
     "build_chart_data",
     "infer_column_roles",
+    "infer_column_semantics",
     "load_dataset",
     "profile_dataset",
+    "roles_from_mapping",
     "suggest_dashboard",
 ]
