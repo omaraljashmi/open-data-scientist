7bdff9fc04cdd5b41a9294ad7663b9713f8b0ce9
omaraljashmi <omaraljashmi.uni@gmail.com>
2026-07-13 21:41:46 -0400

    Milestone 3: export query builder API

--- ods/__init__.py
+++ ods/__init__.py
@@ -13,17 +13,35 @@ from .dashboard import (
     suggest_dashboard,
 )
 from .profiler import DatasetProfile, profile_dataset
+from .query_builder import (
+    AggregateRule,
+    BuiltQuery,
+    FilterRule,
+    QueryBuilderError,
+    QueryResult,
+    QuerySpec,
+    build_query,
+    execute_query,
+)
 from .reporting import build_markdown_report
 
 __all__ = [
     "DatasetLoadError",
     "DatasetProfile",
+    "AggregateRule",
+    "BuiltQuery",
     "ChartSuggestion",
     "ColumnRoles",
     "ColumnSemantic",
     "INTENTS",
+    "FilterRule",
+    "QueryBuilderError",
+    "QueryResult",
+    "QuerySpec",
     "build_markdown_report",
     "build_chart_data",
+    "build_query",
+    "execute_query",
     "infer_column_roles",
     "infer_column_semantics",
     "load_dataset",
